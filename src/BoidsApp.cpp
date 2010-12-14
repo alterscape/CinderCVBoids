a4819d7b2b39e9fa8ccb26175b1f0faffaed87fd
Joanna <Jmerson@asu.edu>
2010-12-13 18:16:44 -0700

    added gravity state - merged with color fade code 3

--- src/BoidsApp.cpp
+++ src/BoidsApp.cpp
@@ -180,7 +180,7 @@ void BoidsApp::setup()
 	
 	// ** stuff to create boid rulesets ** //
 	
-	// State 1: defaults - both flocks are repelled by the sillhoette
+	// State 1: defaults - both flocks are repelled by the silhouette
 	BoidSysPair defaults;
 	defaults.flockOneProps.zoneRadius			= 80.0f;
 	defaults.flockOneProps.lowerThresh			= 0.5f;
@@ -197,7 +197,7 @@ void BoidsApp::setup()
 	defaults.imageColor							= ColorA( 0.08f, 0.0f, 0.1f, 1.0f);	
 	boidRulesets.push_back(defaults);
 	
-	// State 2: stuckOnYou - both flocks are attracted to the sillhoette
+	// State 2: stuckOnYou - both flocks are attracted to the silhouette
 	BoidSysPair stuckOnYou;
 	stuckOnYou.flockOneProps.zoneRadius			= 80.0f;
 	stuckOnYou.flockOneProps.lowerThresh		= 0.5f;
@@ -214,7 +214,7 @@ void BoidsApp::setup()
 	stuckOnYou.imageColor						= ColorA( 0.08f, 0.0f, 0.1f, 1.0f);
 	boidRulesets.push_back(stuckOnYou);
 	
-	//// State 3: diff - one flock is attracted to the sillhoette and the other is repelled
+	//// State 3: diff - one flock is attracted to the silhouette and the other is repelled
 	BoidSysPair diff;
 
 	diff.flockOneProps=defaults.flockOneProps;
@@ -227,6 +227,7 @@ void BoidsApp::setup()
 	BoidSysPair grav;
 	grav.flockOneProps=defaults.flockOneProps;
 	grav.flockOneProps.gravity					= true;
+	grav.flockOneProps.repelStrength		= 0.005f;
 	grav.flockOneProps.baseColor				= ColorA( CM_RGB, 0.157, 0.0, 1.0,1.0);
 	grav.flockTwoProps=grav.flockOneProps;
 	grav.imageColor								= ColorA( 0.08f, 0.0f, 0.1f, 1.0f);
